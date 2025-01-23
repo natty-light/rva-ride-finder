e966bf7c876804c95aa16b0948bf0d82dfc2dfe9
Natasha <75773163+natty-light@users.noreply.github.com>
2025-01-22 21:41:50 -0500

    Register user route

--- rva-ride-finder/src/app/api/users/register-user/route.ts
+++ rva-ride-finder/src/app/api/users/register-user/route.ts
@@ -1,4 +1,6 @@
 import prisma from "@/db/datasource";
+import { adminAuth } from "@/lib/firebase/admin";
+import { getAuthenticatedAppForUser } from "@/lib/firebase/serverApp";
 import { NextResponse } from "next/server";
 
 type RegisterUserRequest = {
@@ -6,22 +8,53 @@ type RegisterUserRequest = {
 }
 
 export async function POST(req: Request) {
-  const { uid } = (await req.json()) as RegisterUserRequest;
 
-  const existingUser = await prisma.user.findFirst({
-    where: {
-      uid
-    }
-  })
+  const idToken = req.headers.get('Authorization')?.split('Bearer')[1].trim()
 
-  if (!existingUser) {
-    console.log(`no existing user found, registering new entry with id: ${uid}`)
-    prisma.user.create({
-      data: {
-        uid,
-      }
-    })
+  if (!idToken) {
+    return NextResponse.json({ message: 'unauthorized' }, { status: 401 })
   }
 
-  NextResponse.json({ message: 'ok' }, { status: 201 })
+  const { currentUser } = await getAuthenticatedAppForUser();
+
+  const decodedToken = await adminAuth.verifyIdToken(idToken)
+
+  if (currentUser?.uid !== decodedToken.uid) {
+    return NextResponse.json({ message: 'unauthorized ' }, { status: 401 })
+  }
+
+
+  try {
+    const { uid } = (await req.json()) as RegisterUserRequest;
+
+    if (!uid) {
+      return NextResponse.json({ message: 'bad request' }, { status: 400 })
+    }
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        uid
+      }
+    })
+    console.log(`found existing user with id ${existingUser?.id}`)
+
+    if (!existingUser) {
+      console.log(`no existing user found, registering new entry with id: ${uid}`)
+      const newUser = await prisma.user.create({
+        data: {
+          uid,
+        }
+      })
+
+      console.log(`created new user with id ${newUser.id}`)
+
+      return NextResponse.json({ message: 'ok' }, { status: 201 })
+    }
+
+    return NextResponse.json({ message: 'user record already exists' }, { status: 200 })
+
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ message: (err as Error).message }, { status: 500 })
+  }
 }
